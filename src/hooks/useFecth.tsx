3f6fc4ec51aaedb475da8ed876808ff373f8594c
lucasnerytoledo <lucas.leite@petlove.com.br>
2023-08-28 14:30:04 -0300

    feat: inserindo hooke useFetch para requisições

---
+++ src/hooks/useFecth.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+export function useFecth<T>(url: RequestInfo, config?: RequestInit) {
+  const [data, setData] = React.useState<T | null>(null)
+  const [loading, setLoading] = React.useState(false)
+  const [error, setError] = React.useState<string | null>(null)
+
+  const configRef = React.useRef(config)
+  configRef.current = config
+
+  React.useEffect(() => {
+    const controller = new AbortController()
+    const { signal } = controller
+    
+    const fetchData = async () => {
+      setLoading(true)
+      setData(null)
+
+      try {
+        const response = await fetch(url, {
+          signal,
+          ...config
+        })
+
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}`)
+        }
+        
+        const data = (await response.json()) as T
+
+        if (!signal.aborted) {
+          setData(data)
+        }
+      } catch (error) {
+        if (!signal.aborted && error instanceof Error) setError(error.message)
+      } finally {
+        if (!signal.aborted) setLoading(false)
+      }
+    }
+    fetchData()
+
+    return () => controller.abort()
+  }, [url, config])
+
+  return { data, loading, error }
+}
