5d23c108804b6b27de752e81945470dd56e450fe
lucasnerytoledo <lucas.leite@petlove.com.br>
2023-08-28 14:20:24 -0300

    base do projeto

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { CreateAccount } from "./pages/CreateAccount";
+import { ResetPassword } from "./pages/ResetPassword";
+import { Profile } from "./pages/Profile";
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/create-account" element={<CreateAccount />}/>
+        <Route path="/reset-password" element={<ResetPassword />}/>
+        <Route path="/profile" element={<Profile />}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
